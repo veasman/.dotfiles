0db4243a09962c41d792aa32c398851c8be94830
Charlie Moren <charlie.moren@softwarfare.com>
2022-07-20 13:36:30 -0500

    feat(emacs): add exwm

--- emacs/.doom.d/packages.el
+++ emacs/.doom.d/packages.el
@@ -54,3 +54,73 @@
   :hook (org-mode . org-bullets-mode)
   :config
   (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(defun cvm/exwm-update-class ()
+  (exwm-workspace-rename-buffer exwm-class-name))
+
+(package! exwm
+  :recipe
+  ;; Set the default number of workspaces
+  (setq exwm-workspace-number 9)
+
+  (exwm-enable)
+
+  ;; When window "class" updates, use it to set the buffer name
+  (add-hook 'exwm-update-class-hook #'cvm/exwm-update-class)
+
+  ;; These keys should always pass through to Emacs
+  (setq exwm-input-prefix-keys
+    '(?\C-x
+      ?\C-u
+      ?\C-d
+      ?\C-h
+      ?\M-`
+      ?\M-&
+      ?\M-:
+      ?\C-\M-j ;; Buffer list
+      ?\C-\ )) ;; Ctrl+SPC
+
+  ;; Ctrl+Q will enable the next key to be sent directly
+  (define-key exwm-mode-map [?\C-q] 'exwm-input-send-next-key)
+
+  ;; Set up global key bindings. These always work, no matter the input state
+  ;; Keep in mind that changing this list after EXWM initalizes has no effect
+  (setq exwm-input-global-keys
+      `(
+        ;; Reset to line-mode C-c C-k switches to char-mode via exwm-input-release-keyboard
+        ([?\s-r] . exwm-reset)
+
+        ;; Move between windows
+        ([?\s-h] . windmove-left)
+        ([?\s-l] . windmove-right)
+        ([?\s-k] . windmove-up)
+        ([?\s-j] . windmove-down)
+
+        ;; Launch applications with shell command
+        ([?\s-p] . (lambda (command)
+                      (interactive (list (read-shell-command "$ ")))
+                      (start-process-shell-command command nil command)))
+
+        ;; Switch workspace
+        ([?\s-w] . exwm-workspace-switch)
+
+        ;; 's-N': Switch to workspace at N
+        ,@(mapcar (lambda (i)
+                    `(,(kbd (format "s-%d" i)) .
+                      (lambda ()
+                        (interactive)
+                        (exwm-workspace-switch-create ,i))))
+                  (number-sequence 0 9))))
+
+  (require 'exwm-randr)
+  (exwm-randr-enable)
+  (start-process-shell-command "xrandr" nil "xrandr --output Virtual1 --primary --mode 1920x1080 -pos 1920x0 --rotate normal --output Virtual2 --mode 1920x1080 --pos 0x0 --rotate normal")
+
+  (setq exwm-randr-workspace-monitor-plist '(1 "Virtual2" 2 "Virtual2" 3 "Virtual2" 4 "Virtual2" 5 "Virtual2"))
+
+  (setq exwm-workspace-warp-cursor t)
+
+  (setq mouse-autoselect-window t
+	focus-follows-mouse t)
+
+  (exwm-enable))
