ff35cd30beb2278b714e204b3d231b3fadf84757
Charlie Moren <charlie.moren@softwarfare.com>
2022-09-07 14:57:04 -0500

    fixes n shit

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -86,3 +86,83 @@
   :hook (org-mode . org-auto-tangle-mode)
   :config
   (setq org-auto-tangle-default t))
+
+(defun cvm/exwm-update-class ()
+  (exwm-workspace-rename-buffer exwm-class-name))
+
+;; Set the default number of workspaces
+(setq exwm-workspace-number 9)
+
+(exwm-enable)
+
+;; When window "class" updates, use it to set the buffer name
+(add-hook 'exwm-update-class-hook #'cvm/exwm-update-class)
+
+;; These keys should always pass through to Emacs
+(setq exwm-input-prefix-keys
+  '(?\C-x
+    ?\C-u
+    ?\C-d
+    ?\C-h
+    ?\M-`
+    ?\M-&
+    ?\M-:
+    ?\C-\M-j ;; Buffer list
+    ?\C-\ )) ;; Ctrl+SPC
+
+;; Ctrl+Q will enable the next key to be sent directly
+(define-key exwm-mode-map [?\C-q] 'exwm-input-send-next-key)
+
+;; Set up global key bindings. These always work, no matter the input state
+;; Keep in mind that changing this list after EXWM initalizes has no effect
+(setq exwm-input-global-keys
+    `(
+      ;; Reset to line-mode C-c C-k switches to char-mode via exwm-input-release-keyboard
+      ([?\s-r] . exwm-reset)
+
+      ;; Move between windows
+      ([?\s-h] . windmove-left)
+      ([?\s-l] . windmove-right)
+      ([?\s-k] . windmove-up)
+      ([?\s-j] . windmove-down)
+
+      ;; Launch applications with shell command
+      ([?\s-p] . (lambda (command)
+                    (interactive (list (read-shell-command "$ ")))
+                    (start-process-shell-command command nil command)))
+
+      ;; Switch workspace
+      ([?\s-w] . exwm-workspace-switch)
+
+      ;; 's-N': Switch to workspace at N
+      ,@(mapcar (lambda (i)
+                  `(,(kbd (format "s-%d" i)) .
+                    (lambda ()
+                      (interactive)
+                      (exwm-workspace-switch-create ,i))))
+                (number-sequence 0 9))))
+
+(require 'exwm-randr)
+(exwm-randr-enable)
+(start-process-shell-command "xrandr" nil "xrandr --output Virtual1 --primary --mode 1920x1080 -pos 0x0 --rotate normal --output Virtual2 --mode 1920x1080 --pos 1920x0 --rotate normal")
+
+(setq exwm-randr-workspace-monitor-plist '(1 "Virtual1" 2 "Virtual1" 3 "Virtual1" 4 "Virtual1" 5 "Virtual1" 6 "Virtual2" 7 "Virtual2" 8 "Virtual2" 9 "Virtual2" 0 "Virtual2"))
+
+(setq exwm-workspace-warp-cursor t)
+
+(setq mouse-autoselect-window t
+      focus-follows-mouse t)
+
+(exwm-enable)
+
+(use-package exwm-modeline
+  :after (exwm)
+  :config
+  (setq exwm-modeline-dividers '("[" "] " "|"))
+  (setq exwm-modeline-short t))
+
+(add-hook 'exwm-init-hook #'exwm-modeline-mode)
+
+(add-hook 'exwm-init-hook #'display-time-mode)
+
+(setq doom-modeline-height 32)
