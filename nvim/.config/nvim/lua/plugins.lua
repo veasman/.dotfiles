b9766ce30f63636001363e28ba7f4ca00f32eb2a
Charlie Moren <charlie.moren@softwarfare.com>
2022-09-16 16:04:18 -0500

    feat: nvim config, emacs changes

---
+++ nvim/.config/nvim/lua/plugins.lua
@@ -0,0 +1,219 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+    PACKER_BOOTSTRAP = fn.system {
+        'git',
+        'clone',
+        '--depth',
+        '1',
+        'https://github.com/wbthomason/packer.nvim',
+        install_path,
+    }
+    print 'Installing packer close and reopen Neovim...'
+    vim.cmd [[packadd packer.nvim]]
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+    augroup packer_user_config
+        autocmd!
+        autocmd BufWritePost plugins.lua source <afile> | PackerSync
+    augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, 'packer')
+if not status_ok then
+    return
+end
+
+-- Have packer use a popup window
+packer.init {
+    display = {
+        open_fn = function()
+            return require('packer.util').float { border = 'rounded' }
+        end,
+    },
+}
+
+return packer.startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use 'nvim-lua/popup.nvim'
+    use 'nvim-lua/plenary.nvim'
+    use 'sheerun/vim-polyglot'
+    use 'nvim-lualine/lualine.nvim'
+    use 'mbbill/undotree'
+    use 'windwp/nvim-autopairs'
+    use 'mattn/emmet-vim'
+    use 'lewis6991/gitsigns.nvim'
+    use 'akinsho/toggleterm.nvim'
+    use 'vimwiki/vimwiki'
+    use 'sunjon/stylish.nvim'
+    use 'christianchiarulli/nvcode-color-schemes.vim'
+    use 'tpope/vim-fugitive'
+    use 'glepnir/dashboard-nvim'
+    use 'Vonr/align.nvim'
+
+    -- Treesitter
+    use 'nvim-treesitter/nvim-treesitter'
+    use 'nvim-treesitter/playground'
+
+    -- Telescope
+    use 'nvim-telescope/telescope.nvim'
+    --use 'nvim-telescope/telescope-media-files.nvim'
+
+    -- Lsp
+    use 'neovim/nvim-lspconfig'
+    use 'williamboman/nvim-lsp-installer'
+    use 'jose-elias-alvarez/null-ls.nvim'
+
+    -- Cmp plugins
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-cmdline'
+    use 'saadparwaiz1/cmp_luasnip'
+    use 'hrsh7th/cmp-nvim-lsp'
+
+    -- Snippets
+    use 'L3MON4D3/LuaSnip'
+    use 'rafamadriz/friendly-snippets'
+
+    use {
+        'folke/trouble.nvim',
+        requires = 'kyazdani42/nvim-web-devicons',
+        config = function()
+            require('trouble').setup()
+        end
+    }
+
+    use {
+        "iamcco/markdown-preview.nvim",
+        run = function()
+            vim.fn["mkdp#util#install"]()
+        end
+    }
+
+    use {
+        'TimUntersberger/neogit',
+        requires = {
+            'nvim-lua/plenary.nvim',
+            'sindrets/diffview.nvim'
+        },
+    }
+
+    use {
+        'petertriho/nvim-scrollbar',
+        config = function()
+            require('scrollbar').setup {
+                handle = {
+                    color = "#1e2127"
+                }
+            }
+        end
+    }
+    use {
+        'windwp/nvim-spectre',
+        requires = 'nvim-lua/plenary.nvim',
+        config = function()
+            require('spectre').setup()
+        end
+    }
+
+    use {
+        'akinsho/bufferline.nvim',
+        tag = "v2.*",
+        requires = 'kyazdani42/nvim-web-devicons'
+    }
+
+    use {
+        'michaelb/sniprun',
+        run = 'bash ./install.sh',
+        requires = 'rcarriga/nvim-notify'
+    }
+
+    use {
+        'filipdutescu/renamer.nvim',
+        branch = 'master',
+        requires = { { 'nvim-lua/plenary.nvim' } },
+        config = function()
+            require('renamer').setup()
+        end
+    }
+
+    use {
+        'norcalli/nvim-colorizer.lua',
+        config = function()
+            require('colorizer').setup()
+        end
+    }
+
+    use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
+    }
+
+    use {
+        'yioneko/nvim-yati',
+        requires = 'nvim-treesitter/nvim-treesitter',
+        config = function()
+            require('nvim-treesitter.configs').setup {
+                yati = { enable = true }
+            }
+        end
+    }
+
+    use {
+        'yamatsum/nvim-cursorline',
+        config = function()
+            require('nvim-cursorline').setup {
+                cursorline = {
+                    timeout = 0
+                },
+            }
+        end
+    }
+
+    use {
+        'weilbith/nvim-code-action-menu',
+        cmd = 'CodeActionMenu',
+    }
+
+    -- Primeagen
+    use 'ThePrimeagen/harpoon'
+    use {
+        'ThePrimeagen/refactoring.nvim',
+        requires = {
+            { 'nvim-lua/plenary.nvim' },
+            { 'nvim-treesitter/nvim-treesitter' }
+        },
+        config = function()
+            require('refactoring').setup()
+        end
+    }
+
+    use {
+        'kyazdani42/nvim-tree.lua',
+        requires = { 'kyazdani42/nvim-web-devicons' },
+    }
+
+    use {
+        'lukas-reineke/indent-blankline.nvim',
+        config = function()
+            require('indent_blankline').setup {
+                show_current_context = true,
+                show_current_context_start = true,
+            }
+        end
+    }
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if PACKER_BOOTSTRAP then
+        require('packer').sync()
+    end
+end)
