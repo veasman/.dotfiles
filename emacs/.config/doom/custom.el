22fa7c2fb80dc4b5ceb04f687c9eeaec5f79ecdf
Charlie <charlie@moren.dev>
2023-02-15 17:21:28 -0600

    feat: yes

--- emacs/.config/doom/custom.el
+++ emacs/.config/doom/custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(exwm-modeline exwm)))
+ '(package-selected-packages '(blamer rainbow-delimiters exwm-modeline exwm)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
