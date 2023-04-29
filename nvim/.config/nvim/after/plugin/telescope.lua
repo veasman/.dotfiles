f775caafbc9b387365960e6e0ca9a7e9600e48ea
Charlie Moren <charlie.moren@softwarfare.com>
2023-04-29 15:25:52 -0500

    windows-wsl2: feat: add nvim config

---
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -0,0 +1,6 @@
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader><leader>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>sp', function()
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,1 @@
+require("cvm")
